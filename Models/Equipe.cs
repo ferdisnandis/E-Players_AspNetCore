0f96c85b734885219d8e18241237606c1531946b
unknown <fe97964@gmail.com>
2020-07-13 11:11:02 -0300

    Inicio do projeto

---
+++ Models/Equipe.cs
@@ -0,0 +1,11 @@
+namespace E_Players_AspNETCore.Models
+{
+    public class Equipe
+    {
+        public int IdEquipe { get; set; }
+        public string Nome { get; set; }
+        public string Imagem { get; set; }
+
+        
+    }
+}
